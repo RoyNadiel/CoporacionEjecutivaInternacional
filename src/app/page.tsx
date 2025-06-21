beceb2011189a3abfda7028c3281c29f7f736b35
RoyNadiel <almadaroy19@gmail.com>
2025-06-21 12:10:26 -0400

    Mobile Page mejorada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,7 @@ export default function Home() {
           </nav>
         </header>
         {/* TITULO Y BANDERAS */}
-        <h1 className="text-3xl md:text-6xl text-center text-yellow-100 font-light mt-2 tracking-wide italic [text-shadow:_-1px_-1px_0_yellow,_1px_-1px_0_yellow,_-1px_1px_0_yellow,_1px_1px_0_yellow]">Corporación Ejecutiva Internacional</h1>
-        <Image className="absolute top-20 md:flex shrink -left-10 hidden" src={"/resources/estatua-libertad.png"} width={300} height={200} alt="Estatua de la Libertad"></Image>
-        <Image className="absolute left-15 top-58 md:hidden" src={"/resources/estatua-libertad.png"} width={60} height={20} alt="Estatua de la Libertad"></Image>
+        <h1 className="text-3xl md:text-6xl text-center text-yellow-100 font-light mt-2 tracking-wide italic [text-shadow:_-1px_-1px_0_yellow,_1px_-1px_0_yellow,_-1px_1px_0_yellow,_1px_1px_0_yellow]">Corporación Ejecutiva Internacional</h1>        
         <div className="grid grid-cols-5 sm:flex sm:w-200 w-fit h-15 gap-2 my-4 justify-evenly items-center">
           <Image src={"/resources/colombia-flag.avif"} width={60} height={150} alt="bandera de colombia"></Image>
           <Image src={"/resources/venezuela-flag.png"} width={60} height={150} alt="bandera de venezuela"></Image>
@@ -24,8 +22,15 @@ export default function Home() {
           <Image src={"/resources/peru-flag.png"} width={60} height={150} alt="bandera de peru"></Image>
           <Image src={"/resources/usa-flag.png"} width={60} height={150} alt="bandera de usa"></Image>
         </div>
+          {/* MOBILE ROSA Y ESTATUA */}
+        <div className="h-fit w-fit flex justify-center items-center gap-15">
+          <Image className="md:flex hidden" src={"/resources/estatua-libertad.png"} width={300} height={200} alt="Estatua de la Libertad"></Image>
+          <Image className="md:hidden" src={"/resources/estatua-libertad.png"} width={60} height={20} alt="Estatua de la Libertad"></Image>
+          <Image className="md:hidden" src={"/resources/rosa-vientos.png"} width={50} height={20} alt={"rosa de los vientos"}></Image>
+        </div>
+
         {/* ROSA DE LOS VIENTOS  */}
-        <Image src={"/resources/rosa-vientos.png"} width={40} height={50} alt={"rosa de los vientos"}></Image>
+        <Image className="md:flex hidden" src={"/resources/rosa-vientos.png"} width={40} height={50} alt={"rosa de los vientos"}></Image>
 
         {/* PRIMER DIV  */}
         <div className="w-fit h-fit flex flex-col items-center justify-evenly px-10 mt-5 md:mt-8 bg-[#9AD2AD] rounded-[80] border border-black ">
@@ -44,7 +49,6 @@ export default function Home() {
             <div className="w-fit h-fit md:p-3 md:flex">
               <button className="text-md md:text-4xl italic cursor-pointer font-extrabold text-[#dee05d]">Ver Cartas Recibidas</button>
               <Image className="md:flex absolute bottom-28 left-165 hidden" src={"/resources/pearl-harbor.jpg"} width={59} height={30} alt="Pearl Harbor"></Image>
-              <Image className="absolute bottom-25 left-45 md:hidden" src={"/resources/pearl-harbor.jpg"} width={40} height={30} alt="Pearl Harbor"></Image>
             </div>
           </div>
           <div className="flex flex-col items-center">
@@ -61,8 +65,11 @@ export default function Home() {
             </div>
           </div>
         </div>
+        <div className="w-fit h-fit">
+          <Image className="md:hidden" src={"/resources/pearl-harbor.jpg"} width={40} height={30} alt="Pearl Harbor"></Image>
+        </div>
         {/* ULTIMO DIV  */}
-        <div className="w-fit h-fit text-center mt-25 md:mt-15 py-3 px-8 rounded-md border-5 border-[#493176] bg-[#F3A8AB]">
+        <div className="w-fit h-fit text-center mt-5 md:mt-15 py-3 px-8 rounded-md border-5 border-[#493176] bg-[#F3A8AB]">
           <h4 className="text-lg md:text-3xl italic font-extrabold text-[#323285]">Pre Fundación Patitas En La Calle</h4>
         </div>
       </div>
